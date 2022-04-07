ce3aac4f6dfd4c2560fa3f0758b1921e17226e9d
lorenuars19 <lorenuar.s19@gmail.com>
2022-04-07 14:30:20 +0200

    first commit

---
+++ lib/gcc/x86_64-apple-darwin18.7.0/11.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../configure --prefix=/Users/lorenuar/bin/ --enable-checking=release --enable-languages=c,c++ --disable-multilib --with-sysroot=/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk --program-suffix=-11.2";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "core2" } };
